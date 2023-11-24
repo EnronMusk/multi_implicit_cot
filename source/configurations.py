181a256f29edc1f5728d30eec4067a2b81c48869
EnronMusk <142634045+EnronMusk@users.noreply.github.com>
2023-11-23 19:20:36 -0500

    first commit

---
+++ source/configurations.py
@@ -0,0 +1,40 @@
+from transformers import PretrainedConfig
+
+class EmulatorConfig(PretrainedConfig):
+    def __init__(
+        self,
+        base_model='gpt2',
+        tokenizer_name='gpt2',
+        mixture_size=1,
+        softmax_temperature=0.05,
+        **kwargs,
+    ):
+        self.base_model = base_model
+        self.tokenizer_name = tokenizer_name
+        self.mixture_size = mixture_size
+        self.softmax_temperature = softmax_temperature
+        super().__init__(**kwargs)
+
+class StudentConfig(PretrainedConfig):
+    def __init__(
+        self,
+        base_model='gpt2',
+        tokenizer_name='gpt2',
+        mixture_size=1,
+        **kwargs,
+    ):
+        self.base_model = base_model
+        self.tokenizer_name = tokenizer_name
+        self.mixture_size = mixture_size
+        super().__init__(**kwargs)
+
+class TeacherConfig(PretrainedConfig):
+    def __init__(
+        self,
+        base_model='gpt2',
+        tokenizer_name='gpt2',
+        **kwargs,
+    ):
+        self.base_model = base_model
+        self.tokenizer_name = tokenizer_name
+        super().__init__(**kwargs)
