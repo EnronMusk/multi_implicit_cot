d0a283427d2fa35cbbeb7b80e3bf01540f48babf
EnronMusk <142634045+EnronMusk@users.noreply.github.com>
2023-11-24 22:09:17 -0500

    updated utils

--- source/utils.py
+++ source/utils.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib as plt
 from transformers import StoppingCriteria, LogitsProcessor
 
 def get_sep_position(input_ids, sep_id, skip=0):
@@ -45,3 +46,23 @@ class DoubleEOSLogitsProcessor(LogitsProcessor):
             scores[done, :] = float('-inf')
             scores[done, self.eos_token_id] = 0
         return scores
+    
+def createAccuracyAndLossPlots(train_losses, train_accs, test_accs):
+    #Plots training summary results on loss function
+    plt.plot(train_losses, color='red', label='train')
+    plt.grid(alpha=0.3)
+    plt.ylabel('loss',fontweight='bold')
+    plt.xlabel('batch',fontweight='bold')
+    plt.title("Training Loss Distribution")
+    plt.legend()
+    plt.show()
+
+    #Plots training summary results on accuracy
+    plt.plot(train_accs, color='red', label='train')
+    plt.plot(test_accs, color='black', label='test')
+    plt.grid(alpha=0.3)
+    plt.ylabel('accuracy',fontweight='bold')
+    plt.xlabel('batch',fontweight='bold')
+    plt.title("Training and Test Accuracy Distribution")
+    plt.legend()
+    plt.show()
